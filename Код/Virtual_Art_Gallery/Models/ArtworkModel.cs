cc797105a364e226eca6254ddba0c0813b83efaa
ShutovaTE <pamada2004@bk.ru>
2024-10-17 11:01:32 +0300

    Добавление кода

---
+++ Код/Virtual_Art_Gallery/Models/ArtworkModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Virtual_Art_Gallery.Models
+{
+    public class ArtworkModel
+    {
+        private int id;
+        private string title;
+        private string description;
+        private bool isPublished;
+        private DateTime dateCreated;
+    }
+}
