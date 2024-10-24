fcc8c98fefba5b038873bfd535ccb7e641e50428
ShutovaTE <pamada2004@bk.ru>
2024-10-24 20:47:19 +0300

    Первая страница

---
+++ Код/Virtual_Art_Gallery/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Virtual_Art_Gallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Код/Virtual_Art_Gallery/obj/Debug/net8.0/Virtual_Art_Gallery.AssemblyInfo.cs
+++ Код/Virtual_Art_Gallery/obj/Debug/net8.0/Virtual_Art_Gallery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Virtual_Art_Gallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cead2783ae276e39546ed6d164a588dfb5208f44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3319571510ac69acd09e9fa29274dd86d730b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Virtual_Art_Gallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Virtual_Art_Gallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
