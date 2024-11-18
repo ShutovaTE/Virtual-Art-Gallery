93cf33ddff45ae37f477bf8e0f01092c4a09fc7a
MaryZhminkovskaya <maryad0102@mail.ru>
2024-11-18 22:08:44 +0300

    Запрет доступа к категориям пользователям

--- Код/Virtual-Art-Gallery рабочий/Controllers/CategoryController.cs
+++ Код/Virtual-Art-Gallery рабочий/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Virtual_Art_Gallery.Models;
 
 namespace Virtual_Art_Gallery.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         private readonly GalleryContext _context;
--- Код/Virtual-Art-Gallery рабочий/obj/Debug/net8.0/Virtual-Art-Gallery.AssemblyInfo.cs
+++ Код/Virtual-Art-Gallery рабочий/obj/Debug/net8.0/Virtual-Art-Gallery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Virtual-Art-Gallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9986f333f2d8f02ab58731dc1998c6647fded9cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4115dad55359a7c89a9f13a9fdf09c252edb62da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Virtual-Art-Gallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Virtual-Art-Gallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
